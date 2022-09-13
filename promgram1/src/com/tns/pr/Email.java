de08d7281aa64c876b5fe1c35e5de40e7842bd28
drakshayani <drakshayani@Reddy>
2022-09-13 14:16:00 +0530

    how many mails for 10 days

---
+++ promgram1/src/com/tns/pr/Email.java
@@ -0,0 +1,17 @@
+package com.tns.pr;
+
+public class Email {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int email = 0;//declare and initialize number of emails received each day 
+for (int day =1;day<=10;day++)
+{
+	System.out.println("Day" +day+":");
+	email++;
+	System.out.println("Inbox has" + email+ "emails"+"\n");
+
+}
+	}
+
+}
