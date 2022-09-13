35d045d08a92d16b2425cb4d75fc6f76b367885d
drakshayani <drakshayani@Reddy>
2022-09-13 11:09:53 +0530

    changed class name

--- B25_Corejava/src/com/tns/prog1/hello.java
+++ B25_Corejava/src/com/tns/prog1/hello.java
@@ -1,10 +1,10 @@
 package com.tns.prog1;
 
-public class Mypack {
+public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hii");
+System.out.println("welcome to corejava");
 	}
 
 }
