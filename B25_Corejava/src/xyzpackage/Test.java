df6d5218eb1f8a5a07412b90f2364323c7f05d45
drakshayani <drakshayani@Reddy>
2022-09-22 11:53:05 +0530

    package

---
+++ B25_Corejava/src/xyzpackage/Test.java
@@ -0,0 +1,15 @@
+package xyzpackage;
+import abcpackage.*;
+public class Test extends Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition obj = new Addition();
+		/*It will throw error because we are trying to access
+		 * the default method in another package
+		 */
+		System.out.println(obj.addTwoNumbers(30, 15));
+
+	}
+
+}
